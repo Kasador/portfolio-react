a635bce9eabe71addbfe1712ddaa3c0b33b2c6a2
Kasador <huntershaw23@gmail.com>
2019-06-16 18:53:06 -0500

    ADDED: added picture of me, plus animations via npm.

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Portfolio.css';
+import styled, { keyframes } from 'styled-components';
+import { fadeInDown } from 'react-animations';
+import ScrollAnimation from 'react-animate-on-scroll';
+import "animate.css/animate.min.css";
+import Me from './images/me.jpeg';
 
-function Portfolio() {
-    return (
-        <>
-            <h1>test</h1>
-        </>
-    );
+const FadeIn = styled.div`animation: 1s ${keyframes`${fadeInDown}`}`;
+
+class Portfolio extends Component {
+    render() {
+        return (
+            <div>
+                <FadeIn>
+                    <h1>I'm Steven.</h1>
+                </FadeIn>
+                <ScrollAnimation animateIn="fadeIn">
+                    <p>testtesttesttesttesttesttesttesttesttesttests</p>
+                    <p>testtesttesttesttesttesttesttesttesttesttests</p>
+                    <p>testtesttesttesttesttesttesttesttesttesttests</p>
+                    <img src={Me} alt="picture of me" />
+                </ScrollAnimation>
+            </div>
+        );
+    }
 }
 
 export default Portfolio;
