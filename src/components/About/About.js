c0b889f50ab746e2bf284a142a6e27aa2da742a5
Kasador <huntershaw23@gmail.com>
2019-06-17 16:29:03 -0500

    ADDED: npm react-responsive. Plus, fixing section headers.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,28 +1,36 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './About.css';
-import styled, { keyframes } from 'styled-components';
-import { fadeInDown } from 'react-animations';
 import "animate.css/animate.min.css";
 import Forest from '../../images/forest-black.png';
 import ScrollAnimation from 'react-animate-on-scroll';
 import Coffee from '../../images/coffee.png';
 import Idaho from '../../images/idaho.png'
 import Hola from '../../images/hola.png';
-
-const FadeInDown = styled.div`animation: 2s ${keyframes`${fadeInDown}`}`;
+import Responsive from 'react-responsive';
+ 
+const Desktop = props => <Responsive {...props} minWidth={992} />;
+const Desktop1 = props => <Responsive {...props} maxWidth={1750} />;
+const Desktop2 = props => <Responsive {...props} maxWidth={1100} />;
+const Tablet = props => <Responsive {...props} minWidth={768} maxWidth={991} />;
+const Mobile = props => <Responsive {...props} maxWidth={767} />;
+const Default = props => <Responsive {...props} minWidth={768} />;
 
 function About() {
     return (
+        <Desktop>
         <div className="About">
-        <ScrollAnimation animateIn="fadeIn" duration="1.5">
-            <img src={Forest} alt="forest art banner" className="ForestBanner" />
+        <ScrollAnimation animateIn="fadeIn" animateOut="fadeOut" duration={1}>
+            <div className="AboutBanner">
+                <h1 className="AboutHeaders">About</h1>
+                <img src={Forest} alt="forest art banner" className="ForestBanner" />
+            </div>
             <div className="AboutContent">
-                <ScrollAnimation animateIn='bounceInLeft' duration="2">
+                <ScrollAnimation animateIn='bounceInLeft' duration={2}>
                 <div className="AboutPictures">
                     <img src={Idaho} alt="coffee cup with the text coder inside" className="AboutIcons"/>
                 </div>
                 </ScrollAnimation>
-                <ScrollAnimation animateIn='bounceInLeft' duration="1.8">
+                <ScrollAnimation animateIn='bounceInLeft' duration={1.8}>
                 <div className="AboutText">
                     <p>test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test</p>
                     <p>test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test</p>
@@ -31,14 +39,14 @@ function About() {
                 </ScrollAnimation>
             </div>
             <div className="AboutContent">
-            <ScrollAnimation animateIn='bounceInLeft' duration="1.8">
+            <ScrollAnimation animateIn='bounceInLeft' duration={1.8}>
                 <div className="AboutText">
                     <p>test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test</p>
                     <p>test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test</p>
                     <p>test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test</p>
                 </div>
             </ScrollAnimation>
-            <ScrollAnimation animateIn='bounceInRight' duration="1.8">
+            <ScrollAnimation animateIn='bounceInRight' duration={2}>
                 <div className="AboutPictures">
                     <img src={Coffee} alt="coffee cup with the text coder inside" className="AboutIcons"/>
                 </div>
@@ -46,12 +54,12 @@ function About() {
             </div>
             
             <div className="AboutContent">
-            <ScrollAnimation animateIn='bounceInLeft' duration="2">
+            <ScrollAnimation animateIn='bounceInLeft' duration={2}>
                 <div className="AboutPictures">
                     <img src={Hola} alt="coffee cup with the text coder inside" className="AboutIcons"/>
                 </div>
             </ScrollAnimation>
-            <ScrollAnimation animateIn='bounceInRight' duration="1.8">
+            <ScrollAnimation animateIn='bounceInRight' duration={1.8}>
                 <div className="AboutText">
                     <p>test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test</p>
                     <p>test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test</p>
@@ -61,6 +69,7 @@ function About() {
             </div>
             </ScrollAnimation>
         </div>
+        </Desktop>
     );
 }
 
