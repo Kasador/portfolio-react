5e22c36e36425226178a3d91f43451567741b3a0
Kasador <huntershaw23@gmail.com>
2019-06-18 02:37:13 -0500

    ADDED: back to top button in footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,11 +2,20 @@ import React, { useState } from 'react';
 import './Footer.css';
 import "animate.css/animate.min.css";
 import FooterImg from '../../images/footer.png';
-import { get } from 'https';
+import { Link, animateScroll as scroll } from "react-scroll";
 
 function Footer() {
     return (
         <div className="Footer">
+            <Link   
+                activeClass="active"
+                to="home"
+                spy={true}
+                smooth={true}
+                offset={-40}
+                duration= {1000} >
+                    <i className="fas fa-chevron-up BackToTop"></i>
+            </Link>
             <img src={FooterImg} alt="footer banner" className="FooterBanner" />
             <span className="FooterText"> Copyright &copy; {new Date().getFullYear()} | H. Steven Shaw</span>
         </div>
