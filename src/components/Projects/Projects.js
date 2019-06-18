cfb5f03b4af8f1b7887ab2171c0060363e9881d5
Kasador <huntershaw23@gmail.com>
2019-06-17 21:44:14 -0500

    IN PROGRESS: making layout to projects section.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,6 +4,7 @@ import "animate.css/animate.min.css";
 import ScrollAnimation from 'react-animate-on-scroll';
 import Forest from '../../images/forest-black-1.png';
 import Responsive from 'react-responsive';
+import Test from '../../images/test.png';
  
 const Desktop = props => <Responsive {...props} minWidth={992} />;
 const Desktop1 = props => <Responsive {...props} maxWidth={1100} />;
@@ -11,7 +12,7 @@ const Tablet = props => <Responsive {...props} minWidth={768} maxWidth={991} />;
 const Mobile = props => <Responsive {...props} maxWidth={767} />;
 const Default = props => <Responsive {...props} minWidth={768} />;
 
-function About() {
+function Projects() {
     return (
         <Desktop>
         <div className="Projects" id="projects">
@@ -21,7 +22,20 @@ function About() {
                 <img src={Forest} alt="mountain art banner" className="ForestOneBanner" />
             </div>
             <div className="ProjectsContent">
-
+                <div className="ProjectsInfo">
+                    <div className="ProjectsCard">
+                        <img src={Test} alt="test image" className="ProjectsImages" />
+                        <button className="ProjectsBtns">Live Link <i class="fas fa-link"></i></button>
+                        <button className="ProjectsBtns">GitHub Link <i class="fab fa-github-alt"></i></button>
+                    </div>
+                    <div className="ProjectsTextWrapper">
+                        <h1 className="ProjectsTitles">Test</h1>
+                        <p className="ProjectsText">test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test </p>
+                        <p className="ProjectsText">test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test </p>
+                        <p className="ProjectsText">test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test </p>
+                    </div>
+                </div>
+                
             </div>
         </ScrollAnimation>
         </div>
@@ -29,4 +43,4 @@ function About() {
     );
 }
 
-export default About;
+export default Projects;
