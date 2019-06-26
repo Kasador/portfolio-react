2d9755cf9771b54f40315a924053b8e8a7c6a26b
Kasador <huntershaw23@gmail.com>
2019-06-26 18:04:20 -0500

    IN PROGRESS: finished mobile menu icon, plus menu, now working on event listener.

--- src/components/NavMobile/NavMobile.js
+++ src/components/NavMobile/NavMobile.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavMobile.css';
-import { bounceInLeft, fadeIn } from 'react-animations';
+import { bounceInLeft, fadeIn, rotateIn } from 'react-animations';
 import styled, { keyframes } from 'styled-components';
 import Usa from '../../images/usa.png';
 import Spain from '../../images/spain.png';
@@ -8,76 +8,94 @@ import { Link } from "react-scroll";
 
 const BounceLeft = styled.div`animation: 1.5s ${keyframes`${bounceInLeft}`}`;
 const FadeIn = styled.div`animation: 2.3s ${keyframes`${fadeIn}`}`;
+const RotateIn = styled.div`animation: 0.5s ${keyframes`${rotateIn}`}`;
 
 function Nav() {
+    const [open, setOpen] = useState(false);
+
+    const Open = () => {
+        setOpen(true);
+    }
+
+    const Close = () => {
+        setOpen(false);
+    }
+
     return (
         <div className="NavMobile">
-            <BounceLeft>
-            <ul className="NavItemsMobile">
-                <Link
-                    activeClass="activeMobile"
-                    to="home"
-                    spy={true}
-                    smooth={true}
-                    offset={-40}
-                    duration= {1000}>
-                        <div className="NavLinksMobile">
-                            <i className="fas fa-tree"></i>
-                            Home
-                        </div>
-                </Link>
-                <Link
-                    activeClass="activeMobile"
-                    to="about"
-                    spy={true}
-                    smooth={true}
-                    offset={-40}
-                    duration= {1000} >
-                        <div className="NavLinksMobile">
-                            <i className="fas fa-address-card"></i>
-                            About
-                        </div>
-                </Link>
-                <Link
-                    activeClass="activeMobile"
-                    to="projects"
-                    spy={true}
-                    smooth={true}
-                    offset={-40}
-                    duration= {500} >
-                        <div className="NavLinksMobile">
-                            <i className="fas fa-project-diagram"></i>
-                            Projects
-                        </div>
-                </Link>
-                <Link
-                    activeClass="activeMobile"
-                    to="contact"
-                    spy={true}
-                    smooth={true}
-                    offset={-40}
-                    duration= {500} >
-                        <div className="NavLinksMobile">
-                            <i className="fas fa-id-card-alt"></i>
-                            Contact
-                        </div>
-                </Link>
-            </ul>
-            </BounceLeft>
             <FadeIn>
-            <div className="LanguagesPosition">
-                <div className="LanguagesWrapper">
-                    <div className="Languages">
-                        <img src={Usa} alt="USA flag" />
-                        <span>English</span>
-                    </div>
-                    <div className="Languages">
-                        <img src={Spain} alt="USA flag" />
-                        <span>Spanish</span>
+                <div className="Plus-Minus">
+                    {!open ? <div onClick={() => Open()}><RotateIn><i className="fas fa-plus-circle Plus-MinusSigns"></i></RotateIn></div> : <RotateIn><div><i className="fas fa-minus-circle Plus-MinusSigns" onClick={() => Close()}></i></div></RotateIn>}
+                </div>
+            </FadeIn>
+            <div className="MobileNavContainer">
+                <BounceLeft>
+                <ul className="NavItemsMobile">
+                    <Link
+                        activeClass="activeMobile"
+                        to="home"
+                        spy={true}
+                        smooth={true}
+                        offset={-40}
+                        duration= {1000}>
+                            <div className="NavLinksMobile">
+                                <i className="fas fa-tree"></i>
+                                Home
+                            </div>
+                    </Link>
+                    <Link
+                        activeClass="activeMobile"
+                        to="about"
+                        spy={true}
+                        smooth={true}
+                        offset={-40}
+                        duration= {1000} >
+                            <div className="NavLinksMobile">
+                                <i className="fas fa-address-card"></i>
+                                About
+                            </div>
+                    </Link>
+                    <Link
+                        activeClass="activeMobile"
+                        to="projects"
+                        spy={true}
+                        smooth={true}
+                        offset={-40}
+                        duration= {500} >
+                            <div className="NavLinksMobile">
+                                <i className="fas fa-project-diagram"></i>
+                                Projects
+                            </div>
+                    </Link>
+                    <Link
+                        activeClass="activeMobile"
+                        to="contact"
+                        spy={true}
+                        smooth={true}
+                        offset={-40}
+                        duration= {500} >
+                            <div className="NavLinksMobile">
+                                <i className="fas fa-id-card-alt"></i>
+                                Contact
+                            </div>
+                    </Link>
+                </ul>
+                </BounceLeft>
+                <FadeIn>
+                <div className="LanguagesPosition">
+                    <div className="LanguagesWrapper">
+                        <div className="Languages">
+                            <img src={Usa} alt="USA flag" />
+                            <span>English</span>
+                        </div>
+                        <div className="Languages">
+                            <img src={Spain} alt="USA flag" />
+                            <span>Spanish</span>
+                        </div>
                     </div>
                 </div>
+                </FadeIn>
             </div>
-            </FadeIn>
         </div>
     )
 }
