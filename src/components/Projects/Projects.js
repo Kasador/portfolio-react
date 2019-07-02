09538b4258b7a1e288b179c4ee27f669c71eaec4
Kasador <huntershaw23@gmail.com>
2019-07-01 19:14:25 -0500

    FIXED: fixed a few errors.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -32,7 +32,7 @@ function Projects() {
                         <div className="ProjectsTextWrapper">
                             <p className="ProjectsText">This is my very first website I've built. I like to keep it unchanged to remind myself how far I've come since then. If I ever feel down about my coding skills, I'll come back to this &#x00026; look at it.</p>
                             <p className="ProjectsText">Originally, it was a website made for Spanish slang. It consists of mostly HTML5 &#x00026; CSS3; however, I did figure out a contact form without knowing php at that time.</p>
-                            <span class="TechHeader">Technologies used:</span>
+                            <span className="TechHeader">Technologies used:</span>
                             <div className="TechUsed">
                                 <i className="fab fa-html5 TechIcons html"></i>
                                 <i className="fab fa-css3-alt TechIcons css"></i>
@@ -48,7 +48,7 @@ function Projects() {
                     <div className="ProjectsTextWrapper">
                         <p className="ProjectsText">This is the first client work I took &#x00026; the first time I got paid for coding something. Really, this was the first real poject This was the beginning of understanding &#x00026; working with clients. Which is a whole other skill by itself.</p>
                         <p className="ProjectsText">The website is made for a detailing company which provides a whole bunch of services. This project pushed me even further with Javascript.</p>
-                        <span class="TechHeader">Technologies used:</span>
+                        <span className="TechHeader">Technologies used:</span>
                         <div className="TechUsed">
                             <i className="fab fa-html5 TechIcons html"></i>
                             <i className="fab fa-css3-alt TechIcons css"></i>
@@ -80,7 +80,7 @@ function Projects() {
                         <div className="ProjectsTextWrapper">
                             <p className="ProjectsText">Another client website; I made this for a newer productions company. They wanted something simple &#x00026; modern. I believe I delieved just that. It was very fun to code &#x00026; I really love the menu on this one.</p>
                             <p className="ProjectsText">I build this using some HTML5, CSS3, ES6, php, &#x00026; MySQL for a simple database that they wanted. Made issue I had was video background, I compressed as best as possible.</p>
-                            <span class="TechHeader">Technologies used:</span>
+                            <span className="TechHeader">Technologies used:</span>
                             <div className="TechUsed">
                                 <i className="fab fa-html5 TechIcons html"></i>
                                 <i className="fab fa-css3-alt TechIcons css"></i>
@@ -97,7 +97,7 @@ function Projects() {
                     <div className="ProjectsTextWrapper">
                         <p className="ProjectsText">This is the first client work I took &#x00026; the first time I got paid for coding something. Really, this was the first real poject This was the beginning of understanding &#x00026; working with clients. Which is a whole other skill by itself.</p>
                         <p className="ProjectsText">The website is made for a detailing company which provides a whole bunch of services. This project pushed me even further with Javascript.</p>
-                        <span class="TechHeader">Technologies used:</span>
+                        <span className="TechHeader">Technologies used:</span>
                         <div className="TechUsed">
                             <i className="fab fa-html5 TechIcons html"></i>
                             <i className="fab fa-css3-alt TechIcons css"></i>
@@ -128,7 +128,7 @@ function Projects() {
                         <div className="ProjectsTextWrapper">
                             <p className="ProjectsText">Another client website; I made this for a newer productions company. They wanted something simple &#x00026; modern. I believe I delieved just that. It was very fun to code &#x00026; I really love the menu on this one.</p>
                             <p className="ProjectsText">I build this using some HTML5, CSS3, ES6, php, &#x00026; MySQL for a simple database that they wanted. Made issue I had was video background, I compressed as best as possible.</p>
-                            <span class="TechHeader">Technologies used:</span>
+                            <span className="TechHeader">Technologies used:</span>
                             <div className="TechUsed">
                                 <i className="fab fa-html5 TechIcons html"></i>
                                 <i className="fab fa-css3-alt TechIcons css"></i>
